d8893d2f7ebd08c73b16aa6d27c88c9799d7e4d3
Lian <cheesetastisch@gmail.com>
2022-10-30 15:12:00 +0100

    General gradle setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,11 @@
-
 rootProject.name = "Core"
 
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
+
+
+include("Api")
