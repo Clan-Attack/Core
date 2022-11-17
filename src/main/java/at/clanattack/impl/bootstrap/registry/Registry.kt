4b85e05b10654caaffe77750cb8aec4a3701f9f1
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2022-11-17 15:12:33 +0100

    Added registry system to ListenerHandler

--- src/main/java/at/clanattack/impl/bootstrap/registry/Registry.kt
+++ src/main/java/at/clanattack/impl/bootstrap/registry/Registry.kt
@@ -2,15 +2,32 @@ package at.clanattack.impl.bootstrap.registry
 
 import at.clanattack.xjkl.scope.asExpr
 
-open class Registry<T : Any>(vararg val possible: Pair<List<Class<out Any>>, List<() -> Any>>) {
+open class Registry<T : Any>(private val possible: MutableList<Pair<List<Class<out Any>>, List<() -> Any>>>) {
+
+    constructor(vararg possible: Pair<List<Class<out Any>>, List<() -> Any>>) : this(possible.toMutableList())
+
+    constructor(`class`: Class<out Any>, get: () -> Any) : this(listOf(`class`) to listOf(get))
+
+    init {
+        possible.add(emptyList<Class<out Any>>() to emptyList())
+    }
 
     private val registry = mutableMapOf<Class<out T>, T>()
 
     protected fun getInstance(`class`: Class<out T>) = registry[`class`]
 
+    protected fun hasInstance(`class`: Class<out T>) = `class` in registry
+
     protected fun registerInstance(instance: T) = this.registerInstance(instance::class.java, instance)
 
+    protected fun registerInstanceOrKeep(instance: T) = this.registerInstanceOrKeep(instance::class.java, instance)
+
     protected fun <U : T> registerInstance(`class`: Class<out U>, instance: U) = asExpr { registry[`class`] = instance }
+
+    protected fun <U : T> registerInstanceOrKeep(`class`: Class<out U>, instance: U) {
+        if (this.hasInstance(`class`)) return
+        registry[`class`] = instance
+    }
 
     protected fun createInstance(`class`: Class<out T>) {
         for ((types, instances) in this.possible) {
@@ -24,6 +41,11 @@ open class Registry<T : Any>(vararg val possible: Pair<List<Class<out Any>>, Lis
         }
 
         throw IllegalStateException("Couldn't register $`class` because the class doesn't have the correct constructor.")
+    }
+
+    protected fun createInstanceOrKeep(`class`: Class<out T>) {
+        if (this.hasInstance(`class`)) return
+        this.createInstance(`class`)
     }
 
 }
--- src/main/java/at/clanattack/impl/utility/listener/ListenerHandler.kt
+++ src/main/java/at/clanattack/impl/utility/listener/ListenerHandler.kt
@@ -1,6 +1,7 @@
 package at.clanattack.impl.utility.listener
 
 import at.clanattack.bootstrap.ICore
+import at.clanattack.impl.bootstrap.registry.Registry
 import at.clanattack.utility.IUtilityServiceProvider
 import at.clanattack.utility.listener.IListenerHandler
 import at.clanattack.utility.listener.ListenerTrigger
@@ -12,9 +13,8 @@ import org.bukkit.event.EventPriority
 import org.bukkit.plugin.EventExecutor
 import java.lang.reflect.Method
 
-class ListenerHandler(private val core: ICore) : IListenerHandler {
-
-    private val instances = mutableMapOf<Class<*>, Any>()
+class ListenerHandler(private val core: ICore) : Registry<Any>(ICore::class.java, { core }), IListenerHandler {
+    
     private val listeners = mutableMapOf<Class<out Event>, MutableList<Pair<Method, Boolean>>>()
 
     private var loadLock = Lock()
@@ -26,45 +26,25 @@ class ListenerHandler(private val core: ICore) : IListenerHandler {
 
     fun loadListeners() {
         this.core.getServiceProvider(IUtilityServiceProvider::class).scopeHandler.async {
-            this@ListenerHandler.core.logger.info("Loading listeners...")
-            this@ListenerHandler.core.annotationScanner.scanMethods(ListenerTrigger::class)
+            core.logger.info("Loading listeners...")
+            core.annotationScanner.scanMethods(ListenerTrigger::class)
                 .forEach {
                     val annotation = it.getAnnotation(ListenerTrigger::class.java)
 
                     val declaringClass = it.declaringClass
                     if (it.parameterCount != 1 || it.parameterTypes[0] != annotation.event.java) {
-                        this@ListenerHandler.core.logger.error(
+                        core.logger.error(
                             "Couldn't register listener for " + annotation.event.simpleName + " in "
                                     + declaringClass.simpleName + " because the method doesn't have the right parameters."
                         )
                     }
 
-                    if (declaringClass in this@ListenerHandler.instances) {
-                        this@ListenerHandler.listeners.putIfAbsent(annotation.event.java, mutableListOf())
-                        this@ListenerHandler.listeners[annotation.event.java]!!.add(it to annotation.subevents)
-                        return@forEach
-                    }
+                    createInstanceOrKeep(declaringClass)
 
-                    val instance = try {
-                        declaringClass.getDeclaredConstructor(ICore::class.java).newInstance(this@ListenerHandler.core)
-                    } catch (e: NoSuchMethodException) {
-                        try {
-                            declaringClass.getDeclaredConstructor().newInstance()
-                        } catch (ex: NoSuchMethodException) {
-                            this@ListenerHandler.core.logger.error(
-                                "Couldn't register listener for " + annotation.event.simpleName + " in "
-                                        + declaringClass.simpleName + " because the class doesn't have the correct constructor."
-                            )
-                            null
-                        }
-                    } ?: return@forEach
-
-                    this@ListenerHandler.instances[declaringClass] = instance
-
-                    this@ListenerHandler.listeners.putIfAbsent(annotation.event.java, mutableListOf())
-                    this@ListenerHandler.listeners[annotation.event.java]!!.add(it to annotation.subevents)
+                    listeners.putIfAbsent(annotation.event.java, mutableListOf())
+                    listeners[annotation.event.java]!!.add(it to annotation.subevents)
                 }
-            this@ListenerHandler.core.logger.info("Loaded ${listeners.size} listeners.")
+            core.logger.info("Loaded ${listeners.size} listeners.")
 
             loadLock.signal()
         }
@@ -75,7 +55,7 @@ class ListenerHandler(private val core: ICore) : IListenerHandler {
         this.core.getServiceProvider(IUtilityServiceProvider::class).scopeHandler.async {
             loadLock.await()
 
-            this@ListenerHandler.core.logger.info("Registering events...")
+            core.logger.info("Registering events...")
 
             val listener: org.bukkit.event.Listener = object : org.bukkit.event.Listener {}
             val executor = EventExecutor { _, event -> fireEvent(event) }
@@ -92,8 +72,8 @@ class ListenerHandler(private val core: ICore) : IListenerHandler {
                     val eventClass = it.loadClass().asSubclass(Event::class.java)
                     if (!eventClass.declaredMethods
                             .any { method -> method.parameterCount == 0 && method.name == "getHandlers" } ||
-                        this@ListenerHandler.listeners.none { (`class`, events) ->
-                            this@ListenerHandler.shouldRegister(
+                        listeners.none { (`class`, events) ->
+                            shouldRegister(
                                 eventClass,
                                 events.map { (_, subevents) -> `class` to subevents })
                         }
@@ -104,13 +84,13 @@ class ListenerHandler(private val core: ICore) : IListenerHandler {
                         listener,
                         EventPriority.NORMAL,
                         executor,
-                        this@ListenerHandler.core.javaPlugin
+                        core.javaPlugin
                     )
 
                     counter++
                 }
 
-            this@ListenerHandler.core.logger.info("Registered $counter Events.")
+            core.logger.info("Registered $counter Events.")
 
             registerLock.signal()
         }
@@ -125,7 +105,7 @@ class ListenerHandler(private val core: ICore) : IListenerHandler {
             .flatten()
             .map { it.first }
             .forEach {
-                val instance = this.instances[it.declaringClass] ?: return@forEach
+                val instance = this.getInstance(it.declaringClass) ?: return@forEach
                 it.invoke(instance, event)
             }
     }
@@ -139,8 +119,6 @@ class ListenerHandler(private val core: ICore) : IListenerHandler {
     private fun shouldRegister(event: Class<out Event>, events: List<Pair<Class<out Event>, Boolean>>) =
         events.any { (methodEvent, subevents) -> shouldCall(event, methodEvent, subevents) }
 
-    override fun registerListenerInstance(instance: Any) {
-        this.instances[instance::class.java] = instance
-    }
+    override fun registerListenerInstance(instance: Any) = this.registerInstance(instance)
 
 }
