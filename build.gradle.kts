02bf0937a666dda7ba52b9f8ace1a765b0c386e0
Lian <cheesetastisch@gmail.com>
2022-11-19 17:32:36 +0100

    Bugfixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     `java-library`
     kotlin("jvm") version "1.7.21"
     id("io.papermc.paperweight.userdev") version "1.3.8"
-    id("net.minecrell.plugin-yml.bukkit") version "0.6"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
