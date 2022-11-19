58bf9b8cf2e67b088fb6cfc7bc088d41ed309a59
Lian <cheesetastisch@gmail.com>
2022-11-19 15:41:58 +0100

    Updated listener annotation system

---
+++ Api/src/main/java/at/clanattack/utility/listener/ListenerPriority.kt
@@ -0,0 +1,11 @@
+package at.clanattack.utility.listener
+
+enum class ListenerPriority {
+
+    HIGHEST,
+    HIGH,
+    NORMAL,
+    LOW,
+    LOWEST
+
+}
--- Api/src/main/java/at/clanattack/utility/listener/ListenerTrigger.kt
+++ Api/src/main/java/at/clanattack/utility/listener/ListenerTrigger.kt
@@ -1,8 +1,32 @@
 package at.clanattack.utility.listener
 
 import org.bukkit.event.Event
+import org.jetbrains.annotations.ApiStatus.ScheduledForRemoval
 import kotlin.reflect.KClass
 
+@MustBeDocumented
 @Target(AnnotationTarget.FUNCTION)
 @Retention(AnnotationRetention.RUNTIME)
-annotation class ListenerTrigger(val event: KClass<out Event>, val subevents: Boolean = false)
+annotation class ListenerTrigger(
+    val event: KClass<out Event>,
+
+    @Deprecated("Replace with @IncludeSubevents")
+    @ScheduledForRemoval(inVersion = "0.7")
+    val subevents: Boolean = false
+)
+
+@MustBeDocumented
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Priority(val priority: ListenerPriority)
+
+@MustBeDocumented
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ExecuteCanceled
+
+@MustBeDocumented
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class IncludeSubevents
+
